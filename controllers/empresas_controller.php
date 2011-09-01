1a7d8cf1fae43d42beb3860be40d8e4370a7166c
Demetriux <davidpsj@davidpsj.net>
2011-08-31 21:21:41 -0300

    Alterações: controllers\empresas_controller.php views\layouts\default.ctp
    Inclusões:
    views\tipo_lancamento\index.php
    
    Signed-off-by: Demetriux <davidpsj@davidpsj.net>

--- controllers/empresas_controller.php
+++ controllers/empresas_controller.php
@@ -4,6 +4,16 @@ class EmpresasController extends AppController {
 	var $name = 'Empresas';
 	var $scaffold;
 	
+	//TODO: Implementar e criar as wiews
+	function index(){}
+	
+	function _listarEmpresas(){}
+	
+	function novo(){}
+	
+	function editar($id = null){}
+	
+	function excluir($id){}
 }
 
 ?>
---
+++ views/tipo_lancamento/index.php
@@ -0,0 +1,5 @@
+<?php echo $title_for_layout?>
+
+<h2><?echo __('Tipos de Lançamentos');?></h2>
+
+<p><?php echo __('O que você deseja fazer?')?></p>
